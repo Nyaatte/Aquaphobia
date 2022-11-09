1a0d367c260d895b0a5a3f349f9d6cc96db8466d
Nyaatte <kirbyhenry99@gmail.com>
2022-11-09 14:21:31 +1300

    movement working VR

--- ahhhh!_this_water_is_scary!/Assets/Henry Sandbox/Scripts/Hologram.cs
+++ ahhhh!_this_water_is_scary!/Assets/Henry Sandbox/Scripts/Hologram.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Events;
-
+using Valve.VR.InteractionSystem;
 
 [RequireComponent(typeof(Collider))]
 public class Hologram : MonoBehaviour
@@ -11,13 +11,10 @@ public class Hologram : MonoBehaviour
 
     public GameObject hologram;
     public GameObject hologramReferencePoint;
-
+    public Interactable Interactable;
     public Vector3 currentPosition;
 
-    private Vector3 mouseScroll;
-    [SerializeField] float zPosition;
-
-[SerializeField]
+    [SerializeField]
     private bool isControllerDefaultPosition;
 
 
@@ -26,93 +23,27 @@ public class Hologram : MonoBehaviour
     // Start is called before the first frame update
     private void Awake()
     {
-        //Cursor.lockState = CursorLockMode.Locked;
-        zPosition = transform.position.z;
         positionDifference = new Vector3(0f, 0f, 0f);
         isControllerDefaultPosition = true;
     }
 
     private void Update()
     {
-        /*
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (hologram == null)
-            {
-                RaycastHit hit = CastRay();
-
-                if (hit.collider != null)
-                {
-                    if (!hit.collider.CompareTag("hologram"))
-                    {
-                        return;
-                    }
-
-                    hologram = hit.collider.gameObject;
-
-                }
-            }
-            
-
-            else
-            { //when the hologram is released from hand
-                if (isControllerDefaultPosition)
-                {
-                    hologram.transform.position = hologramReferencePoint.transform.position;
-                    hologram = null;
-                    positionDifference = new Vector3(0, 0, 0);
-                }
-                else{
-                    hologram.transform.position = currentPosition;
-                    hologram = null;
-                }
-            }
-            
-        }
-        */
-
-        
-            
-
-            /*
-            Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(hologram.transform.position).z);
-            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
-            worldPosition.z += mouseScroll.y;
-            hologram.transform.position = new Vector3(Mathf.Clamp(worldPosition.x, hologramReferencePoint.transform.position.x - 10, hologramReferencePoint.transform.position.x + 10), Mathf.Clamp(worldPosition.y, hologramReferencePoint.transform.position.y - 10, hologramReferencePoint.transform.position.y + 10), Mathf.Clamp(worldPosition.z, hologramReferencePoint.transform.position.z - 10, hologramReferencePoint.transform.position.z + 10));
-            */
-            
+           /*
             currentPosition = hologram.transform.position;
-            positionDifference = hologramReferencePoint.transform.position - currentPosition;
-        
-        /*
-        if (Input.GetKey("s"))
-        {
-            isControllerDefaultPosition = !isControllerDefaultPosition;
-        }
-        */
+            positionDifference = transform.TransformPoint(hologramReferencePoint.transform.position) - transform.TransformPoint(currentPosition);
+          */
+
+            if (Interactable.attachedToHand)
+         {
+             currentPosition = this.transform.parent.position;
+             positionDifference = hologramReferencePoint.transform.position - currentPosition;
+         }
+         else
+         {
+             positionDifference = new Vector3(0, 0, 0);
+         }
     }
-
-    /*
-    //Ignore this method, only for mouse.
-    private RaycastHit CastRay()
-    {
-        Vector3 screenmousePosFar = new Vector3(
-            Input.mousePosition.x,
-            Input.mousePosition.y,
-            Camera.main.farClipPlane);
-
-        Vector3 screenmousePosNear = new Vector3(
-        Input.mousePosition.x,
-        Input.mousePosition.y,
-        Camera.main.nearClipPlane);
-
-        Vector3 worldMousePosFar = Camera.main.ScreenToWorldPoint(screenmousePosFar);
-        Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenmousePosNear);
-        RaycastHit hit;
-        Physics.Raycast(worldMousePosNear, worldMousePosFar - worldMousePosNear, out hit);
-
-        return hit;
-    }
-    */
 }
+           
+
